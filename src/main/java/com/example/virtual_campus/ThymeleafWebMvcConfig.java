3bb9896e3f672f03678e2c0b26337a8cc6617299
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-04-23 22:57:38 +0200

    Resolved exception with "PersistenceAnnotationBeanPostProcessor" after running project. Problem was with dependencies.

--- src/main/java/com/example/virtual_campus/ThymeleafWebMvcConfig.java
+++ src/main/java/com/example/virtual_campus/ThymeleafWebMvcConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
