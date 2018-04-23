523a1f0da413275e65855304ce9d60925fe60649
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-04-23 23:52:38 +0200

    Preparing security

--- src/main/java/com/example/virtual_campus/MappingController.java
+++ src/main/java/com/example/virtual_campus/MappingController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class MappingController {
 
-    @GetMapping("/blank")
+    @GetMapping("/index")
     public String blank(){
-        return "blank";
+        return "index";
     }
 
-    @GetMapping("faculties")
+    @GetMapping("/faculties")
     public String faculties(){
         return "faculties";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
