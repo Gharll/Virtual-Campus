5e9ecbb33d6608520e2453e8448b771dc4164747
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-05-08 18:55:30 +0200

    Working login for default account.

--- src/main/java/com/example/virtual_campus/config/SpringSecurityConfig.java
+++ src/main/java/com/example/virtual_campus/config/SpringSecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.virtual_campus.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
@@ -19,30 +24,44 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/home", "/about").permitAll()
+        http.headers().frameOptions().disable();
+        
+        /*
+        *
+        *       .antMatchers("/", "/home", "/about").permitAll()
+                .antMatchers("/faculties").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/vendor/**", "/dist/**", "/js/**", "/less/**", "/data/**").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("USER")
-                .anyRequest().authenticated()
-                .and()
+        * */
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
                 .logout()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
+                    .permitAll()
+                    .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(customAccessDeniedHandler);
     }
 
-    // create two users, admin and user
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @SuppressWarnings("deprecation")
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
 
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("ADMIN");
+        return new InMemoryUserDetailsManager(user);
     }
 }
