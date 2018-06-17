e16ac1ab49114cc8437332eb51892a6db89a90d3
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-06-17 19:33:57 +0200

    Activation university_role for users

--- src/main/java/com/example/virtual_campus/controller/RestController/ActivationRestController.java
+++ src/main/java/com/example/virtual_campus/controller/RestController/ActivationRestController.java
@@ -1,38 +1,47 @@
 package com.example.virtual_campus.controller.RestController;
 
 import com.example.virtual_campus.model.Student;
+import com.example.virtual_campus.model.UniversityRole;
 import com.example.virtual_campus.model.User;
 import com.example.virtual_campus.repository.StudentRepository;
+import com.example.virtual_campus.repository.UniversityRoleRepository;
 import com.example.virtual_campus.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RestController
+@RequestMapping("/api")
 public class ActivationRestController {
 
     @Autowired
-    private StudentRepository studentRepository;
+    private UniversityRoleRepository universityRoleRepository;
 
     @Autowired
     private UserRepository userRepository;
 
-    @PutMapping("put/active/students/{activationCode}")
-    public Student update(@PathVariable(value = "activationCode") String activationCode,
+    @PutMapping("put/active/{activationCode}")
+    public UniversityRole update(@PathVariable(value = "activationCode") String activationCode,
                           @Valid Student studentDetails) {
 
-        Student student = studentRepository.findByActivationCode(activationCode);
+        UniversityRole universityRole = universityRoleRepository.findByActivationCode(activationCode);
+        if(universityRole != null && !universityRole.isActive()){
+            UserDetails userDetails =
+                    (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = userRepository.findByEmail(userDetails.getUsername());
 
-        UserDetails userDetails =
-                (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User user = userRepository.findByEmail(userDetails.getUsername());
+            universityRole.setActive(true);
+            universityRole.setUser(user);
 
-        student.setActive(true);
-        student.setUser(user);
+            return universityRoleRepository.save(universityRole);
+        }
 
-        return studentRepository.save(student);
+        return null;
     }
 }
