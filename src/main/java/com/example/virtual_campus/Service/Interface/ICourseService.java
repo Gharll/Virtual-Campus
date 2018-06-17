ef1ff89d8e538df252522209b9483816ed3cfded
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-06-18 00:22:49 +0200

    Delete unused files from service package

--- src/main/java/com/example/virtual_campus/Service/Interface/ICourseService.java
+++
@@ -1,7 +0,0 @@
-package com.example.virtual_campus.Service.Interface;
-
-import com.example.virtual_campus.model.Course;
-
-public interface ICourseService {
-    void save(Course course);
-}
--- src/main/java/com/example/virtual_campus/Service/Interface/ISubjectService.java
+++
@@ -1,9 +0,0 @@
-package com.example.virtual_campus.Service.Interface;
-
-import com.example.virtual_campus.model.Subject;
-
-public interface ISubjectService {
-
-    Subject findByName(String name);
-    void save(Subject subject);
-}
